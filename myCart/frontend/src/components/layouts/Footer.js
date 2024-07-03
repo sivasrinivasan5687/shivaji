9cca878bfccb2195e312a90b8addabcb380d9e2e
sivasrinivasan5687 <sivasrinivasan5687@gmail.com>
2024-07-03 18:49:03 +0530

    =deleted

--- myCart/frontend/src/components/layouts/Footer.js
+++
@@ -1,9 +0,0 @@
-export  default function Footer (){
-    return (
-        <footer className="py-1">
-            <p className="text-center text-dark mt-1">
-                JVLcart - 2022-2023, All Rights Reserved
-            </p>
-        </footer>
-    )
-}
